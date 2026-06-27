207af85e4ae06bfe74b01ee78a891c19aa7f97a7
Gerardo Venegas <gmatiasvc@hotmail.com>
2026-06-27 10:39:32 -0500

    Added landing page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,10 @@
 import Alpine from 'alpinejs';
+import { createIcons, icons } from 'lucide';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+document.addEventListener('DOMContentLoaded', () => {
+    createIcons({ icons });
+});
